a9d30fafaf19e2d960020b70e70ecc8929926cfe
AxOrSha2 <90012640+AxOrSha2@users.noreply.github.com>
2022-03-13 18:31:41 -0500

    Update To See current correctly in home and shop

--- store/src/app/app-routing.module.ts
+++ store/src/app/app-routing.module.ts
@@ -8,12 +8,24 @@ import { Page404Component } from './components/page404/page404.component';
 import { RegisterUsersComponent } from './components/register-users/register-users.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'shopping-cart', component: ShoppingCartComponent },
-  { path: 'create-user', component: RegisterUsersComponent },
-  { path: 'alter-user/:id', component: RegisterUsersComponent },
-  { path: 'user', component: UserComponent },
-  { path: '404', component: Page404Component },
+  { path: '', 
+    component: HomeComponent 
+  },
+  { path: 'shopping-cart', 
+    component: ShoppingCartComponent 
+  },
+  { path: 'create-user', 
+    component: RegisterUsersComponent 
+  },
+  { path: 'alter-user/:id', 
+    component: RegisterUsersComponent 
+  },
+  { path: 'user', 
+    component: UserComponent 
+  },
+  { path: '404', 
+    component: Page404Component
+  },
   { path: '**', redirectTo: '404', pathMatch: 'full' }
 ];
 
